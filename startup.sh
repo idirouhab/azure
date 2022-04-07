5dee0f5bbf3d1c1bd6b0f35ec695ba1f0faba9bb
Idir Ouhab Meskine <ouhab.idir@gmail.com>
2022-04-07 11:54:25 +0200

    first-commit

---
+++ startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://github.com/signalfx/signalfx-dotnet-tracing/releases/download/v0.2.2/signalfx-dotnet-tracing_0.2.2_amd64.deb
+dpkg -i signalfx-dotnet-tracing_0.2.2_amd64.deb
